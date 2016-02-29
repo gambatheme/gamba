16f13d598a9280765341c83935a0a7dd4e86a9e1
gambatheme <gambatheme@gmail.com>
2016-02-29 14:12:17 +0700

    Added files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+jQuery(function($){
+    "use strict";
+
+    var SLZ = window.SLZ || {};
+    
+
+    /*=======================================
+    =            SAMPLE FUNCTION            =
+    =======================================*/
+
+    SLZ.sampleFunction = function(){
+
+    }
+
+    /*=====  End of SAMPLE FUNCTION  ======*/
+
+
+
+
+    /*======================================
+    =            INIT FUNCTIONS            =
+    ======================================*/
+    
+    $(document).ready(function(){
+        SLZ.sampleFunction();
+    });
+    
+    /*=====  End of INIT FUNCTIONS  ======*/
+    
+   
+});
+
